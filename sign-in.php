21caaf1b26ab6ff999f432f0f37ed42f3c95a36c
Sanjana <shettysanjana09@gmail.com>
2023-11-11 10:03:41 +0530

    first commit

---
+++ sign-in.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./sign-in.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Satoshi:wght@500&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=DM Sans:wght@700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Farro:wght@700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Lato:wght@400;800&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400&display=swap"
+    />
+  </head>
+  <body>
+    <div class="sign-in">
+      <div class="container3">
+        <div class="main6">
+          <div class="form-registerdefault">
+            <div class="btnsolidlarge">
+              <div class="title-1">Create your account</div>
+            </div>
+            <div class="form1">
+              <div class="btnsolidlarge">
+                <div class="inputlarge2">
+                  <div class="form-titledefault2">
+                    <div class="form-title2">Full name</div>
+                  </div>
+                  <div class="input2">
+                    <div class="container4">
+                      <div class="control2">
+                        <img class="icon8" alt="" src="./public/icon1.svg" />
+
+                        <div class="text3">Enter your name</div>
+                      </div>
+                    </div>
+                    <img
+                      class="select-arrow-icon2"
+                      alt=""
+                      src="./public/select-arrow.svg"
+                    />
+
+                    <img
+                      class="icon-right2"
+                      alt=""
+                      src="./public/icon-right.svg"
+                    />
+                  </div>
+                  <div class="form-help-text4">
+                    <div class="form-help-text5">This is a helper text</div>
+                  </div>
+                </div>
+              </div>
+              <div class="btnsolidlarge">
+                <div class="inputlarge2">
+                  <div class="form-titledefault2">
+                    <div class="form-title2">E-mail or phone number</div>
+                  </div>
+                  <div class="input2">
+                    <div class="container4">
+                      <div class="control2">
+                        <img class="icon8" alt="" src="./public/icon1.svg" />
+
+                        <div class="text3">
+                          Type your e-mail or phone number
+                        </div>
+                      </div>
+                    </div>
+                    <img
+                      class="select-arrow-icon2"
+                      alt=""
+                      src="./public/select-arrow.svg"
+                    />
+
+                    <img
+                      class="icon-right2"
+                      alt=""
+                      src="./public/icon-right.svg"
+                    />
+                  </div>
+                  <div class="form-help-text4">
+                    <div class="form-help-text5">This is a helper text</div>
+                  </div>
+                </div>
+              </div>
+              <div class="btnsolidlarge">
+                <div class="inputlarge2">
+                  <div class="form-titledefault2">
+                    <div class="form-title2">Password</div>
+                  </div>
+                  <div class="input2">
+                    <div class="container4">
+                      <div class="control2">
+                        <img class="icon8" alt="" src="./public/icon1.svg" />
+
+                        <div class="text3">Type your password</div>
+                      </div>
+                    </div>
+                    <img
+                      class="select-arrow-icon2"
+                      alt=""
+                      src="./public/select-arrow.svg"
+                    />
+
+                    <img
+                      class="icon-right2"
+                      alt=""
+                      src="./public/icon-right.svg"
+                    />
+                  </div>
+                  <div class="form-help-text8">
+                    <div class="form-help-text5">
+                      Must be 8 characters at least
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="check">
+                <img
+                  class="checkboxsingledefault-icon"
+                  alt=""
+                  src="./public/checkboxsingledefault.svg"
+                />
+
+                <div class="text-">
+                  <span>By creating an account means you agree to the </span>
+                  <b>Terms</b>
+                  <span> </span>
+                  <b>and</b>
+                  <span> </span>
+                  <b>Conditions</b>
+                  <span>, and our </span>
+                  <b>Privacy Policy</b>
+                </div>
+              </div>
+              <div class="x-button">
+                <div class="btnsolidlarge">
+                  <div class="btnlarge">
+                    <div class="elements">
+                      <img class="icon8" alt="" src="./public/icon2.svg" />
+
+                      <div class="text-button">Sign Up</div>
+                      <img
+                        class="chevron-right-icon"
+                        alt=""
+                        src="./public/chevronright.svg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="social">
+              <div class="delimiter">
+                <div class="delimiter-child"></div>
+                <div class="or-do-it">or do it via other accounts</div>
+                <div class="delimiter-child"></div>
+              </div>
+              <div class="social1">
+                <img class="social-child" alt="" src="./public/group-344.svg" />
+
+                <div class="social-child">
+                  <div class="group-child18"></div>
+                  <img
+                    class="apple-logo-black-1-icon"
+                    alt=""
+                    src="./public/apple-logo-black-1.svg"
+                  />
+                </div>
+                <div class="social-child">
+                  <div class="group-child19"></div>
+                  <b class="f">f</b>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="button6" id="popupbuttonContainer">
+            <b class="label6">Sign Up</b>
+          </div>
+          <div class="label-wrapper">
+            <div class="label7">
+              <span>Aldready have an account?</span>
+              <span class="span26"> </span>
+              <span class="sign-in1">Sign In</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="aside1">
+        <div class="sidenav-bg2"></div>
+        <img
+          class="divider-vertical-icon1"
+          alt=""
+          src="./public/dividervertical.svg"
+        />
+
+        <b class="welcome-to-wisebugdet2">Welcome to WiseBugdet!</b>
+        <div class="button7" id="popupbuttonContainer1">
+          <img class="shape-icon7" alt="" src="./public/shape1.svg" />
+
+          <div class="square7">WiseBugdet</div>
+        </div>
+      </div>
+      <img
+        class="ant-designpicture-filled-icon1"
+        alt=""
+        src="./public/antdesignpicturefilled.svg"
+      />
+    </div>
+
+    <script>
+      var popupbuttonContainer = document.getElementById("popupbuttonContainer");
+      if (popupbuttonContainer) {
+        popupbuttonContainer.addEventListener("click", function (e) {
+          window.location.href = "./home-page.html";
+        });
+      }
+      
+      var popupbuttonContainer1 = document.getElementById("popupbuttonContainer1");
+      if (popupbuttonContainer1) {
+        popupbuttonContainer1.addEventListener("click", function (e) {
+          window.location.href = "./index1.html";
+        });
+      }
+      </script>
+  </body>
+</html>
